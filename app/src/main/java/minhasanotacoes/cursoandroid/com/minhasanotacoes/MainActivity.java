884cfb5969055f5881b57ff0832b36ad2ba5744f
salatielaizza <salatiel.aizza@gmail.com>
2017-01-12 18:17:41 +0000

    Android MinhasAnotacoes

---
+++ app/src/main/java/minhasanotacoes/cursoandroid/com/minhasanotacoes/MainActivity.java
@@ -0,0 +1,111 @@
+package minhasanotacoes.cursoandroid.com.minhasanotacoes;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText texto;
+    private Button botaoSalvar;
+    private static final String NOME_ARQUIVO = "arquivo_anotação.txt";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto = (EditText) findViewById(R.id.textoID);
+        botaoSalvar = (Button) findViewById(R.id.botaoSalvarID);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textoDigitado = texto.getText().toString();
+                gravarNoArquivo( textoDigitado );
+                Toast.makeText(MainActivity.this, "Texto Salvo com sucesso", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if (lerArquivo() != null) {
+
+            texto.setText(lerArquivo());
+
+        }
+    }
+
+
+    //método que grava o que foi digitado
+    public void gravarNoArquivo(String texto) {
+
+        try {
+
+            //instanciar a classe OutputStreamWriter e no construtor da classe passamos o nome do arquivo.txt
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter( openFileOutput( NOME_ARQUIVO, Context.MODE_PRIVATE));
+
+            //utilizamos o método write para escrever o arquivo
+            outputStreamWriter.write( texto );
+
+            //utilizamos o método close para encerrar o arquivo
+            outputStreamWriter.close();
+
+        }catch (IOException e){
+
+            Log.v( "Mainactivity", e.toString() );
+
+        }
+    }
+
+
+    //método que lê o arquivo e recupera o texto digitado
+    public String lerArquivo () {
+
+        String resultado = " ";
+
+        try {
+
+            //Abrir o arquivo
+            InputStream arquivo = openFileInput(NOME_ARQUIVO);
+
+            //testar se o aquivo existe
+            if( arquivo != null ){
+
+                //ler o arquivo
+                InputStreamReader inputStreamReader = new InputStreamReader( arquivo );
+
+                //Gerar Buffer do arquivo lido para poder recuperar as informações contidas no arquivo
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                //Recuperar textos do arquivo
+                String linhaArquivo = " ";
+                while((linhaArquivo = bufferedReader.readLine()) != null ) {
+                    resultado += linhaArquivo;
+                }
+
+                arquivo.close();
+
+            }
+        }catch (IOException e) {
+
+            Log.v("Mainactivity", e.toString());
+
+        }
+
+        return resultado;
+
+    }
+}
